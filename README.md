4bda5541a8501c9bf5a3d241042c6002c7816f60
Xavier S <spartan9077@gmail.com>
2022-01-29 14:25:32 -0700

    added return and button

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # week1
-Create REPO CSC3916_HW0 <br/>
+
+<!-- Create REPO CSC3916_HW0 <br/>
 https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token <br />
 
 git clone https://github.com/shawnmccarthy/week1.git <br/>
@@ -8,6 +9,6 @@ git init <br/>
 git add remote https://github.com/YOUNAME/week1.git <br/>
 git add . <br/>
 git commit -m "first commit" <br/>
-git push origin main <br/>
+git push origin main <br/> -->
 
-[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/3701afbe3191ae18ab24)
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/fca8234f0711c330313d?action=collection%2Fimport#?env%5BHW_0%5D=W3sia2V5IjoiYm9va190aXRsZSIsInZhbHVlIjoiIiwiZW5hYmxlZCI6dHJ1ZSwidHlwZSI6ImRlZmF1bHQiLCJzZXNzaW9uVmFsdWUiOiJUdXJpbmciLCJzZXNzaW9uSW5kZXgiOjB9LHsia2V5IjoiaWQiLCJ2YWx1ZSI6IiIsImVuYWJsZWQiOnRydWUsInR5cGUiOiJkZWZhdWx0Iiwic2Vzc2lvblZhbHVlIjoibUtudUR3QUFRQkFKIiwic2Vzc2lvbkluZGV4IjoxfV0=)
--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -1,20 +1,33 @@
-const axios = require('axios')
+const axios = require("axios");
 
 module.exports = async (phrase) => {
-    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
-        params: {
-            format: 'json',
-            q: `${phrase}`
-        }
-    })
+  const results = await axios.get(
+    "https://www.googleapis.com/books/v1/volumes",
+    {
+      params: {
+        format: "json",
+        q: 'Turing',//`${phrase}`,
+      },
+    }
+  );
 
-    return JSON.stringify(results.data);
-}
+  // console.log(results.data);
+  // console.log(results.status);
+  // console.log(results.statusText);
+  // console.log(results.headers);
 
-/*  console.log(results.data);
-    console.log(results.status);
-    console.log(results.statusText);
-    console.log(results.headers);
+  let returnThis = {
+      data: results.data,
+      status: results.status,
+      statusText: results.statusText,
+      headers: results.headers,
+      requestHeader: results.config.headers,
+  }
+
+  return JSON.stringify(returnThis);
+};
+
+/*  
 
         const results = await axios({
         method: 'get',
